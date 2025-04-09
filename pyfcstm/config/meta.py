af4a20353e5b8f3cc4896e510e27b1bc123eb282
HansBug <hansbug@buaa.edu.cn>
2025-04-09 14:28:57 +0800

    dev(hansbug): init commit

---
+++ pyfcstm/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for pyfcstm package.
+"""
+
+#: Title of this project (should be `pyfcstm`).
+__TITLE__ = 'pyfcstm'
+
+#: Version of this project.
+__VERSION__ = '0.10.0'
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Useful utilities for huggingface'
+
+#: Author of this project.
+__AUTHOR__ = 'hansbug1992'
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = 'hansbug1992@hansbug.org'

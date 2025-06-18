1e67abf4247ecc38f11cc79ffab65345abe335d4
HansBug <hansbug@buaa.edu.cn>
2025-06-18 17:48:30 +0800

    dev(hansbug): refactor rendering code

--- pyfcstm/render/__init__.py
+++ pyfcstm/render/__init__.py
@@ -1,3 +1,3 @@
 from .env import create_env
 from .expr import render_expr_node, fn_expr_render, add_expr_render_to_env
-from .render import render_from_template_dir
+from .render import StateMachineCodeRenderer
--- pyfcstm/render/render.py
+++ pyfcstm/render/render.py
@@ -1,40 +1,94 @@
-import glob
 import os.path
 import pathlib
 import shutil
 import warnings
-from typing import Optional, Dict
+from functools import partial
+from typing import Optional, Dict, Callable
+
+import yaml
 
 from .env import create_env
 from .expr import add_expr_render_to_env
 from ..model import StateMachine
 
 
-def render_from_template_dir(model: StateMachine, template_dir: str, output_dir: str,
-                             lang_style: str = 'dsl', ext_configs: Optional[Dict[str, str]] = None,
-                             clear_previous_directory: bool = False):
-    output_dir = os.path.abspath(output_dir)
+class StateMachineCodeRenderer:
+    def __init__(self, template_dir: str, config_file: str = 'config.yaml'):
+        self.template_dir = os.path.abspath(template_dir)
+        self.config_file = os.path.join(self.template_dir, config_file)
 
-    env = create_env()
-    env = add_expr_render_to_env(env, lang_style=lang_style, ext_configs=ext_configs)
+        self._lang_style: Optional[str] = None
+        self._lang_ext_configs: Optional[dict] = None
+        self._prepare_for_configs()
 
-    os.makedirs(output_dir, exist_ok=True)
-    if clear_previous_directory:
-        for file in os.listdir(output_dir):
-            dst_file = os.path.join(output_dir, file)
-            if os.path.isfile(dst_file):
-                os.remove(dst_file)
-            elif os.path.isdir(dst_file):
-                shutil.rmtree(dst_file)
-            elif os.path.islink(dst_file):
-                os.unlink(dst_file)
-            else:
-                warnings.warn(f'Unable to clean file {dst_file!r}.')
+        self._file_mappings: Dict[str, Callable] = {}
+        self._prepare_for_file_mapping()
 
-    for template_file in glob.glob(os.path.join(template_dir, '**', '*.j2'), recursive=True):
-        rel_file = os.path.splitext(os.path.relpath(template_file, template_dir))[0]
-        tp = env.from_string(pathlib.Path(template_file).read_text())
-        dst_file = os.path.join(output_dir, rel_file)
-        os.makedirs(os.path.dirname(dst_file), exist_ok=True)
-        with open(dst_file, 'w') as f:
+        self.env = create_env()
+        self.env = add_expr_render_to_env(
+            self.env,
+            lang_style=self._lang_style,
+            ext_configs=self._lang_ext_configs,
+        )
+
+    def _prepare_for_configs(self):
+        with open(self.config_file, 'r') as f:
+            config_info = yaml.safe_load(f)
+
+        expr_style = config_info.pop('expr_style', 'dsl')
+        if isinstance(expr_style, str):
+            self._lang_style = expr_style
+            self._lang_ext_configs = {}
+        else:
+            self._lang_style = expr_style.pop('base_lang', 'dsl')
+            self._lang_ext_configs = expr_style
+
+    def _prepare_for_file_mapping(self):
+        for root, _, files in os.walk(self.template_dir):
+            for file in files:
+                _, ext = os.path.splitext(file)
+                current_file = os.path.abspath(os.path.join(root, file))
+                rel_file = os.path.splitext(os.path.relpath(current_file, self.template_dir))[0]
+                if ext == '.j2':
+                    self._file_mappings[rel_file] = partial(
+                        self.render_one_file,
+                        template_file=current_file,
+                    )
+                elif not os.path.samefile(current_file, self.config_file):
+                    self._file_mappings[rel_file] = partial(
+                        self.copy_one_file,
+                        src_file=current_file,
+                    )
+
+    def render_one_file(self, model: StateMachine, output_file: str, template_file: str):
+        tp = self.env.from_string(pathlib.Path(template_file).read_text())
+        if os.path.dirname(output_file):
+            os.makedirs(os.path.dirname(self.config_file), exist_ok=True)
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        with open(output_file, 'w') as f:
             f.write(tp.render(model=model))
+
+    def copy_one_file(self, model: StateMachine, output_file: str, src_file: str):
+        _ = model
+        if os.path.dirname(output_file):
+            os.makedirs(os.path.dirname(self.config_file), exist_ok=True)
+        shutil.copyfile(src_file, output_file)
+
+    def render(self, model: StateMachine, output_dir: str, clear_previous_directory: bool = False):
+        output_dir = os.path.abspath(output_dir)
+        os.makedirs(output_dir, exist_ok=True)
+        if clear_previous_directory:
+            for file in os.listdir(output_dir):
+                dst_file = os.path.join(output_dir, file)
+                if os.path.isfile(dst_file):
+                    os.remove(dst_file)
+                elif os.path.isdir(dst_file):
+                    shutil.rmtree(dst_file)
+                elif os.path.islink(dst_file):
+                    os.unlink(dst_file)
+                else:
+                    warnings.warn(f'Unable to clean file {dst_file!r}.')
+
+        for rel_file, fn_op in self._file_mappings.items():
+            dst_file = os.path.join(output_dir, rel_file)
+            fn_op(model=model, output_file=dst_file)

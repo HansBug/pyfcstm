8fbd6b4b817819f3336cc27e8288f3039d27d9c8
HansBug <hansbug@buaa.edu.cn>
2025-06-21 16:06:12 +0800

    dev(hansbug): add pyfcstm generate CLI

--- pyfcstm/entry/generate.py
+++ pyfcstm/entry/generate.py
@@ -1,6 +1,11 @@
+import pathlib
+
 import click
 
 from .base import CONTEXT_SETTINGS
+from ..dsl import parse_with_grammar_entry
+from ..model import parse_dsl_node_to_state_machine
+from ..render import StateMachineCodeRenderer
 
 
 def _add_generate_subcommand(cli: click.Group) -> click.Group:
@@ -15,6 +20,17 @@ def _add_generate_subcommand(cli: click.Group) -> click.Group:
     @click.option('--clear', '--clear-directory', 'clear_directory', type=bool, is_flag=True,
                   help='Clear the destination directory of the output directory.')
     def generate(input_code_file, template_dir, output_dir, clear_directory):
-        pass
+        code = pathlib.Path(input_code_file).read_text()
+        ast_node = parse_with_grammar_entry(code, entry_name='state_machine_dsl')
+        model = parse_dsl_node_to_state_machine(ast_node)
+
+        renderer = StateMachineCodeRenderer(
+            template_dir=template_dir,
+        )
+        renderer.render(
+            model,
+            output_dir=output_dir,
+            clear_previous_directory=clear_directory
+        )
 
     return cli

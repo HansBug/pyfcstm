3ae6bafea73e61ad1d1fb6e788a8de6e11f3d456
HansBug <hansbug@buaa.edu.cn>
2025-09-25 19:58:43 +0800

    dev(hansbug): fix plantuml generation

---
+++ test/model/test_model_dlc4.py
@@ -0,0 +1,30 @@
+import pathlib
+
+import pytest
+
+from pyfcstm.dsl import parse_with_grammar_entry
+from pyfcstm.model.model import *
+from test.testings import get_testfile
+
+
+@pytest.fixture()
+def demo_model_1():
+    dsl_code = pathlib.Path(get_testfile('dlc4_dsl_code.fcstm')).read_text()
+    ast_node = parse_with_grammar_entry(dsl_code, entry_name='state_machine_dsl')
+    model = parse_dsl_node_to_state_machine(ast_node)
+    return model
+
+
+@pytest.fixture()
+def demo_model_1_plantuml():
+    plantuml_code = pathlib.Path(get_testfile('dlc4_plantuml.puml')).read_text()
+    return plantuml_code
+
+
+@pytest.mark.unittest
+class TestModelModelDLC4:
+    def test_plantuml_generation(self, demo_model_1, demo_model_1_plantuml, text_aligner):
+        text_aligner.assert_equal(
+            expect=demo_model_1_plantuml,
+            actual=demo_model_1.to_plantuml(),
+        )

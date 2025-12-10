3a189fdf06a5b58691f1d1f992ecffdadad21038
HansBug <hansbug@buaa.edu.cn>
2025-12-10 22:57:18 +0800

    dev(hansbug): add convert_sysdesim CLI

--- pyfcstm/entry/cli.py
+++ pyfcstm/entry/cli.py
@@ -1,3 +1,4 @@
+from .convert_sysdesim import _add_convert_sysdesim_subcommand
 from .dispatch import pyfcstmcli
 from .generate import _add_generate_subcommand
 from .plantuml import _add_plantuml_subcommand
@@ -5,6 +6,7 @@ from .plantuml import _add_plantuml_subcommand
 _DECORATORS = [
     _add_generate_subcommand,
     _add_plantuml_subcommand,
+    _add_convert_sysdesim_subcommand,
 ]
 
 cli = pyfcstmcli

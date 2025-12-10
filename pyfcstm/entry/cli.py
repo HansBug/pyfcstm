b5f6e1e94c89f73f8d41c09dada78380b13be786
HansBug <hansbug@buaa.edu.cn>
2025-12-10 23:15:30 +0800

    dev(hansbug): add CLI for reachability

--- pyfcstm/entry/cli.py
+++ pyfcstm/entry/cli.py
@@ -2,11 +2,13 @@ from .convert_sysdesim import _add_convert_sysdesim_subcommand
 from .dispatch import pyfcstmcli
 from .generate import _add_generate_subcommand
 from .plantuml import _add_plantuml_subcommand
+from .reachability import _add_reachability_subcommand
 
 _DECORATORS = [
     _add_generate_subcommand,
     _add_plantuml_subcommand,
     _add_convert_sysdesim_subcommand,
+    _add_reachability_subcommand,
 ]
 
 cli = pyfcstmcli

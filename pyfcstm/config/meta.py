124421b09827ff47b54efbe619d7fcdde6da255d
HansBug <hansbug@buaa.edu.cn>
2026-03-13 17:28:46 +0800

    Bump version to 0.3.0

--- pyfcstm/config/meta.py
+++ pyfcstm/config/meta.py
@@ -19,7 +19,7 @@ Example::
     >>> meta.__TITLE__
     'pyfcstm'
     >>> meta.__VERSION__
-    '0.2.1'
+    '0.3.0'
 
 .. note::
    These values are intended to be constants and should not be modified at
@@ -31,7 +31,7 @@ Example::
 __TITLE__: str = 'pyfcstm'
 
 #: Version of this project.
-__VERSION__: str = '0.2.1'
+__VERSION__: str = '0.3.0'
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__: str = (

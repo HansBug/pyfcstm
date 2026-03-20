7b6e7fbc4edc39bb7447b666a4552274b63731cd
HansBug <hansbug@buaa.edu.cn>
2026-03-21 00:06:41 +0800

    Add coverage for C-safe identifier utility

--- test/utils/test_text.py
+++ test/utils/test_text.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pyfcstm.utils import normalize, to_identifier
+from pyfcstm.utils import normalize, to_c_identifier, to_identifier
 
 
 @pytest.mark.unittest
@@ -53,3 +53,91 @@ class TestIdentifierConversion:
 
     def test_trailing_underscore_removal(self):
         assert to_identifier("hello_world_") == "hello_world"
+
+    @pytest.mark.parametrize(
+        "input_string, strict_mode, expected",
+        [
+            ("Hello World", True, "Hello_World"),
+            ("hello_world", True, "hello_world"),
+            ("class", True, "class_"),
+            ("Class", True, "Class_"),
+            ("template", True, "template_"),
+            ("Template", True, "Template_"),
+            ("namespace", True, "namespace_"),
+            ("compl", True, "compl_"),
+            ("xor_eq", True, "xor_eq_"),
+            ("thread_local", True, "thread_local_"),
+            ("noexcept", True, "noexcept_"),
+            ("alignas", True, "alignas_"),
+            ("static_assert", True, "static_assert_"),
+            ("operator", True, "operator_"),
+            ("union", True, "union_"),
+            ("volatile", True, "volatile_"),
+            ("auto", True, "auto_"),
+            ("bool", True, "bool_"),
+            ("true", True, "true_"),
+            ("false", True, "false_"),
+            ("nullptr", True, "nullptr_"),
+            ("requires", True, "requires_"),
+            ("concept", True, "concept_"),
+            ("and", True, "and_"),
+            ("or", True, "or_"),
+            ("not", True, "not_"),
+            ("bitor", True, "bitor_"),
+            ("char32_t", True, "char32_t_"),
+            ("wchar_t", True, "wchar_t_"),
+            ("_Atomic", True, "_Atomic_"),
+            ("_Thread_local", True, "_Thread_local_"),
+            ("123_abc", True, "_123_abc"),
+            ("!@#$%^&*()_+", True, "_empty"),
+            ("namespace_", True, "namespace_"),
+            ("class-name", True, "class_name"),
+            ("friend class", True, "friend_class"),
+            ("中文 class", True, "Zhong_Wen_class"),
+            ("Hello World", False, "Hello_World"),
+            ("hello_world", False, "hello_world"),
+            ("class", False, "class_"),
+            ("Class", False, "Class_"),
+            ("template", False, "template_"),
+            ("Template", False, "Template_"),
+            ("namespace", False, "namespace_"),
+            ("compl", False, "compl_"),
+            ("xor_eq", False, "xor_eq_"),
+            ("thread_local", False, "thread_local_"),
+            ("noexcept", False, "noexcept_"),
+            ("alignas", False, "alignas_"),
+            ("static_assert", False, "static_assert_"),
+            ("operator", False, "operator_"),
+            ("union", False, "union_"),
+            ("volatile", False, "volatile_"),
+            ("auto", False, "auto_"),
+            ("bool", False, "bool_"),
+            ("true", False, "true_"),
+            ("false", False, "false_"),
+            ("nullptr", False, "nullptr_"),
+            ("requires", False, "requires_"),
+            ("concept", False, "concept_"),
+            ("and", False, "and_"),
+            ("or", False, "or_"),
+            ("not", False, "not_"),
+            ("bitor", False, "bitor_"),
+            ("char32_t", False, "char32_t_"),
+            ("wchar_t", False, "wchar_t_"),
+            ("_Atomic", False, "_Atomic_"),
+            ("_Thread_local", False, "_Thread_local_"),
+            ("123_abc", False, "123_abc"),
+            ("!@#$%^&*()_+", False, ""),
+            ("namespace_", False, "namespace_"),
+            ("class-name", False, "class_name"),
+            ("friend class", False, "friend_class"),
+            ("中文 class", False, "Zhong_Wen_class"),
+        ],
+    )
+    def test_to_c_identifier(self, input_string, strict_mode, expected):
+        assert to_c_identifier(input_string, strict_mode) == expected
+
+    def test_to_c_identifier_leaves_non_reserved_names_unchanged(self):
+        assert to_c_identifier("hello_world") == "hello_world"
+
+    def test_to_c_identifier_applies_reserved_check_after_normalization(self):
+        assert to_c_identifier("namespace_") == "namespace_"
